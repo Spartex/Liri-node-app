1715099dff2d9bc43b12c3f269ed37dc39080a34
John.C <johncomo13@gmail.com>
2018-08-28 20:16:23 -0700

    Made small changes and cleaned up some code

--- liri.js
+++ liri.js
@@ -1,9 +1,10 @@
-    // Node module imports needed to run the functions
+// Node module imports needed to run the functions
 require("dotenv").config();
 
 var request = require("request");
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
+var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
 var args = process.argv.slice(2);
@@ -14,20 +15,21 @@ var userInput = args.slice(1).join(" ");
 if (command === "spotify-this-song") {
 	spotifyThis();
 } else if (command === "movie-this") {
-    movieThis();
+	movieThis();
 }
-//else if (command === "concert-this") {
-        //concertThis();
-//} 
+else if (command === "concert-this") {
+	concertThis();
+}
 else if (command === "do-what-it-says") {
 	fileSaysDo();
+
 } else {
 	console.log("OOOPS! I don't understand . Please tell me a command: \nspotify-this-song \nmovie-this \nconcert-this  \ndo-what-it-says");
 }
 
 
 function spotifyThis() {
-	if(!userInput) {
+	if (!userInput) {
 		userInput = "The Sign Ace of Base";
 	}
 
@@ -36,7 +38,7 @@ function spotifyThis() {
 		type: "track",
 		query: userInput,
 		limit: 1
-	}, function(err, data) {
+	}, function (err, data) {
 		if (err) {
 			return console.log(err);
 		} else {
@@ -47,11 +49,11 @@ function spotifyThis() {
 			console.log("Album: " + data.tracks.items[0].album.name) // album name
 		}
 
-		fs.appendFile("log.txt", "\nAppending this song and artist data: " + 
-			"\n" + data.tracks.items[0].album.artists[0].name + 
-			"\n" + data.tracks.items[0].name + 
-			"\n" + data.tracks.items[0].album.external_urls.spotify + 
-			"\n" + data.tracks.items[0].album.name, function(err) {
+		fs.appendFile("log.txt", "\nAppending this song and artist data: " +
+			"\n" + data.tracks.items[0].album.artists[0].name +
+			"\n" + data.tracks.items[0].name +
+			"\n" + data.tracks.items[0].album.external_urls.spotify +
+			"\n" + data.tracks.items[0].album.name, function (err) {
 				if (err) {
 					console.log(err);
 				}
@@ -60,17 +62,17 @@ function spotifyThis() {
 }
 
 function movieThis() {
-	if(userInput === null){
+	if (userInput === null) {
 		movieName = "Mr. Nobody";
 	}
-		
-	
+
+
 	var queryUrl = "http://www.omdbapi.com/?apikey=e146aa24&t=" + userInput + "&tomatoes=true&plot=short&r=json";
 
-	request(queryUrl, function(err, response, body) {
-		if (!err && response.statusCode=== 200) {
-			console.log ("Everything is look good!!");
-		} 
+	request(queryUrl, function (err, response, body) {
+		if (!err && response.statusCode === 200) {
+			console.log("Everything is looking good!!");
+		}
 
 		console.log("---------------------------------------------");
 		console.log("Movie Title: " + JSON.parse(body)["Title"]);
@@ -82,15 +84,15 @@ function movieThis() {
 		console.log("Movie Language : " + JSON.parse(body)["Language"]);
 		console.log("Movie Plot : " + JSON.parse(body)["Plot"]);
 		console.log("Movie Actors : " + JSON.parse(body)["Actors"]);
-		
-	
-	
 
-		fs.appendFile("log.txt", "\n" + "Appending this movie information: " + 
-			"\n" + JSON.parse(body).Title + "\n" + JSON.parse(body).Year + 
+
+
+
+		fs.appendFile("log.txt", "\n" + "Appending this movie information: " +
+			"\n" + JSON.parse(body).Title + "\n" + JSON.parse(body).Year +
 			"\n" + JSON.parse(body).imdbRating + "\n" + JSON.parse(body).tomatoRating +
 			"\n" + JSON.parse(body).Country + "\n" + JSON.parse(body).Language +
-			"\n" + JSON.parse(body).Plot + "\n" + JSON.parse(body).Actors, function(err) {
+			"\n" + JSON.parse(body).Plot + "\n" + JSON.parse(body).Actors, function (err) {
 				if (err) {
 					console.log(err);
 				}
@@ -98,26 +100,29 @@ function movieThis() {
 	})
 }
 
-//Not able to get data printed from bandsintown API for some reason
+//Not able to get data printed from bandsintown API
 
-//function concertThis() {
-	//var queryUrl = ("https://rest.bandsintown.com/artists" + userInput + "/events?app_id=codingbootcamp"); 
+function concertThis() {
+	var queryUrl = ("https://rest.bandsintown.com/artists" + userInput + "/events?app_id=codingbootcamp");
 
-	//request(queryUrl, function(err, response, events) {
-		//if (!err && response.statusCode=== 200) {
-			//console.log ("Everything is look good!!");
-		//} 
-		//console.log("---------------------------------------------");
-		//console.log("Band Name: " + JSON.parse(events)["name"]);
-		//console.log("Band Name: " + JSON.parse["venue.name"]);
+	request(queryUrl, function (err, response, body) {
+		if (!err && response.statusCode === 200) {
+			console.log(JSON.parse(body));
+		}
 
-	//})
-//}
+		console.log("---------------------------------------------");
+		console.log("Band Name: " + (JSON.parse(body)[0].name));
+		console.log("Venue Name: " + (JSON.parse(body)[0].venue.name));
+		console.log("Venue City: " + (JSON.parse(body)[0].venue.city));
+		console.log(moment(JSON.parse[0].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 
+
+	});
+}
 
 function fileSaysDo() {
-	fs.readFile("random.txt", "utf8", function(error, data) {
+	fs.readFile("random.txt", "utf8", function (error, data) {
 		if (error) {
 			return console.log(error);
 		} else {
@@ -125,17 +130,17 @@ function fileSaysDo() {
 			userInput = dataArr[1];
 			command = dataArr[0];
 
-			 if (command === "spotify-this-song") {
+			if (command === "spotify-this-song") {
 				spotifyThis();
-            } if (command === "movie-this") {
+			} if (command === "movie-this") {
 				movieThis();
 			}
-			 //if (command === "concert-this") {
+			if (command === "concert-this") {
 				concertThis();
-			//}
+			}
 		}
 
-		fs.appendFile("log.txt", "User has engaged the random file.", function(err) {
+		fs.appendFile("log.txt", "User has engaged the random file.", function (err) {
 			if (err) {
 				console.log(err);
 			}
